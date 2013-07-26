de27fd833b07ba0e2c641b09ed9a33ba54581338
Ivo Anjo <ivo.anjo@ist.utl.pt>
2013-07-26 20:09:17 +0100

    Update runtest.sh script for latest intel sde version (6.1.0-2013-07-22)

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # this is specific to my machine, modify this accordingly
-SDE_DIR=~/projects/intel-htm/sde-bdw-external-5.38.0-2013-01-03-lin/
+SDE_DIR=~/projects/intel-htm/sde-external-6.1.0-2013-07-22-lin/
 
-$SDE_DIR/sde64 -rtm_mode full -rtm_debug_log 1 -rtm_stats 1 -rtm_stats_call_stack 1 -rtm_cache_set_size 128 -- java javartm.Test
+$SDE_DIR/sde64 -rtm_mode full -tsx 1 -tsx_debug_log 1 -tsx_stats 1 -tsx_stats_call_stack 1 -tsx_cache_set_size 128 -- java javartm.Test
